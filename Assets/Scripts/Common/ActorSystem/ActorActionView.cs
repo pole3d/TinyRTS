0bf73dacce97954acc70c38ee7b9e404e6031bb4
aurelienesteves <71756157+aurelienesteves@users.noreply.github.com>
2023-10-04 17:25:50 +0200

    Import raw actor system

---
+++ Assets/Scripts/Common/ActorSystem/ActorActionView.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Common.ActorSystem
+{
+    /// <summary>
+    /// View d'une actor action
+    /// </summary>
+    [Serializable]
+    public class ActorActionView
+    {
+        public string Name;
+        //public SpriteAnimationView Animation;
+        public string Sound;
+        //public float AnimationSpeed;
+        public string NextAction;
+
+
+        [HideInInspector]
+        public float CurrentTimer;
+    }
+
+}
