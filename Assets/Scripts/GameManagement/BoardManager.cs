73484bef329ddcb6e9ec382a5c776a18ac3b443f
P3D\matteo.benaissa <matteobenaissa@gmail.com>
2023-10-09 14:49:04 +0200

    Ressource manager

---
+++ Assets/Scripts/GameManagement/BoardManager.cs
@@ -0,0 +1,15 @@
+namespace GameManagement
+{
+    public class BoardManager 
+    {
+        public BoardManager()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }        
+    }
+}
--- Assets/Scripts/GameManagement/GameManager.cs
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GameManagement.Players;
+using UnityEngine;
 using Utilities;
 
 namespace GameManagement
@@ -21,6 +22,14 @@ namespace GameManagement
     
     public class GameManager : Singleton<GameManager>
     {
+        //editor fields
+        [Space(5), Header("Teams")]
+        [SerializeField] private PlayerTeamEnum _playerTeam;
+        [SerializeField] private PlayerTeamEnum _enemyTeam;
+        
+        //references
+        public BoardManager Board { get; private set; }
+        
         //datas
         public GameManagerGameData GameData { get; private set; }
         
@@ -35,13 +44,19 @@ namespace GameManagement
         
         protected override void InternalAwake()
         {
+            if (_playerTeam == _enemyTeam)
+            {
+                throw new Exception("Player's and Enemy's team are the same");
+            }
             GameData = new GameManagerGameData(
-                new LocalPlayerManager(PlayerTeamEnum.Human, 0), 
-                new AIPlayerManager(PlayerTeamEnum.Orcs, 1));
+                new LocalPlayerManager(_playerTeam, 0), 
+                new AIPlayerManager(_enemyTeam, 1));
             
             OnGameStarted += GameStart;
             OnGameWon += GameWon;
             OnGameLost += GameLost;
+
+            Board = new BoardManager();
         }
 
         protected override void OnDestroy()
@@ -61,6 +76,8 @@ namespace GameManagement
         private void Update()
         {
             UpdateTaskManagement();
+            
+            Board?.Update();
         }
 
         #region Task Management
