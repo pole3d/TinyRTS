7224e741467408f580e3c3b88e8d513640388caf
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-11-08 20:54:26 +0100

    target to the closest if no pathfinding find

--- Assets/Scripts/Pathfinding/PathFinding.cs
+++ Assets/Scripts/Pathfinding/PathFinding.cs
@@ -26,11 +26,21 @@ internal class PathFinding
         _grid = new ShowGrid<PathNode>(width, height, cellSize, Vector3.zero, (ShowGrid<PathNode> grid, int x, int y) => new PathNode(x, y), parent);
     }
 
+    public void ResetNodeWalkable(List<Vector3> listPos, int index)
+    {
+        if (listPos.Count > 0)
+            SetPathReserved(listPos, index);
 
-    public void SetNodeWalkable(List<Vector3> listPos, int index, bool isWalkable)
+        if (listPos.Count > 1 && index < listPos.Count - 1)
+        {
+            //next
+            SetPathReserved(listPos, index + 1);
+        }
+    }
+    public void SetPathReserved(List<Vector3> listPos, int index, GameObject owner = null)
     {
         GetGrid().GetXY(listPos[index], out int x, out int y);
-        GetNode(x, y).SetIsWalkable(isWalkable);
+        GetNode(x, y).ReservedPath(owner);
     }
     public List<Vector3> FindPath(Vector3 startWorldPosition, Vector3 endWorldPosition)
     {
@@ -60,9 +70,7 @@ internal class PathFinding
 
         startNode.GCost = 0;
         startNode.HCost = CalculateDistance(startNode, endNode);
-        startNode.CalculateFCost();
-
-
+        startNode.FCost = startNode.CalculateFCost();
 
         while (_openList.Count > 0)
         {
@@ -91,7 +99,7 @@ internal class PathFinding
                 {
                     neighbourNode.GCost = tentativeGCost;
                     neighbourNode.HCost = CalculateDistance(neighbourNode, endNode);
-                    neighbourNode.CalculateFCost();
+                    neighbourNode.FCost = neighbourNode.CalculateFCost();
                     neighbourNode.cameFromNode = currentNode;
                     if (_openList.Contains(neighbourNode) == false) _openList.Add(neighbourNode);
                 }
@@ -99,6 +107,10 @@ internal class PathFinding
             }
 
         }
+        if (_closestToTarget != null)
+            return CalculatePath(_closestToTarget);
+
+
         return null;
     }
 
@@ -161,14 +173,23 @@ internal class PathFinding
 
         return neighbour;
     }
+
+
     private PathNode GetLowestFCostNode(List<PathNode> openList)
     {
         PathNode lowestFCost = openList[0];
 
         foreach (var item in openList)
         {
-            if (lowestFCost.FCost < item.FCost) lowestFCost = item;
+            if (lowestFCost.FCost > item.FCost)
+            {
+                lowestFCost = item;
+            }
+
+            if (_closestToTarget != null && _closestToTarget.HCost > item.HCost) _closestToTarget = item;
+            else if (_closestToTarget == null) _closestToTarget = item;
         }
+
         return lowestFCost;
     }
     private void InitGrid()
@@ -182,7 +203,7 @@ internal class PathFinding
                 pathNode.Neighbours = GetNeighbour(pathNode);
 
                 pathNode.GCost = int.MaxValue;
-                pathNode.CalculateFCost();
+                pathNode.FCost = pathNode.CalculateFCost();
                 pathNode.cameFromNode = null;
             }
         }
