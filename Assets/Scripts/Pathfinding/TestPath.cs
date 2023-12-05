9d0494151da5be4281d0f782487909fde11a22fa
LEPERS_NICOLAS\nicol <nicolaslepers176@gmail.com>
2023-12-05 17:39:14 +0100

    Gizmos + line red for wall

--- Assets/Scripts/Pathfinding/TestPath.cs
+++ Assets/Scripts/Pathfinding/TestPath.cs
@@ -9,6 +9,7 @@ public class TestPath : MonoBehaviour
     [SerializeField] int _height;
     [SerializeField] float _cellSize;
     [SerializeField] Transform _textParent;
+    private List<Vector3> _pathVectorList = new List<Vector3>();
 
     [SerializeField] List<CharacterPathfindingMovementHandler> characters;
 
@@ -31,12 +32,11 @@ public class TestPath : MonoBehaviour
             var time = Time.realtimeSinceStartupAsDouble;
             for (int i = 0; i < NBPath; i++)
             {
-                PathFinding.Instance.FindPath(Vector3.zero, GetMouseWorldPos());
+                _pathVectorList = PathFinding.Instance.FindPath(Vector3.zero, GetMouseWorldPos());
             }
             Debug.Log(time - Time.realtimeSinceStartupAsDouble + "ms" + nb_calculate + " nombre de fois");
             nb_calculate++;
         }
-
 
         if (Input.GetMouseButtonDown(1))
         {
@@ -44,6 +44,7 @@ public class TestPath : MonoBehaviour
             Vector3 mouseWorldPos = GetMouseWorldPos();
             _pathFinding.Grid.GetXY(mouseWorldPos, out int x, out int y);
             _pathFinding.Grid.GetGridObject(x, y).SetIsWalkable(0);
+            Debug.DrawLine(new Vector3(mouseWorldPos.x, mouseWorldPos.y - 2, 0), new Vector3(mouseWorldPos.x,mouseWorldPos.y+2,0),Color.red, 100f);
         }
     }
 
@@ -58,4 +59,24 @@ public class TestPath : MonoBehaviour
         Vector3 worldPos = worldCam.ScreenToWorldPoint(screenPos);
         return worldPos;
     }
+
+    private void OnDrawGizmos()
+    {
+        foreach (var item in PathFinding.Instance.OpenList)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawSphere(new Vector3((item.Coordinates.x * 5) + 5 / 2, (item.Coordinates.y * 5) + 5 / 2, 0), 2);
+        }
+        foreach (var item in PathFinding.Instance.ClosedList)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(new Vector3((item.Coordinates.x * 5) + 5 / 2, (item.Coordinates.y * 5) + 5 / 2, 0), 2);
+        }
+        foreach (var item in _pathVectorList)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(new Vector3(item.x, item.y, 0), 2);
+        }
+    }
+
 }
