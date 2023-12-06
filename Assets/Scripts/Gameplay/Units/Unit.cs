6ed62601bd9e86f61667460cb58c344930c93b61
Simon Walenczak <92367148+SimonWalenczak@users.noreply.github.com>
2023-12-06 16:54:48 +0100

    fix unit test

--- Assets/Scripts/Gameplay/Units/Unit.cs
+++ Assets/Scripts/Gameplay/Units/Unit.cs
@@ -1,62 +1,65 @@
 using Common.ActorSystem;
 using UnityEngine;
 
-/// <summary>
-/// Represents a unit in the rts,
-/// the unit can respond to order, like moving
-/// a unit can be hit and die
-/// </summary>
-public class Unit : MonoBehaviour
+namespace Gameplay.Units
 {
-    public int Life { get; private set; }
-    public int Damage => _data.Damage;
-    public int Range => _data.Range;
-
-    [SerializeField] ActorView _view;
-
-    UnitData _data;
-    Vector2? _destination;
-
-    public void Initialize(UnitData data)
+    /// <summary>
+    /// Represents a unit in the rts,
+    /// the unit can respond to order, like moving
+    /// a unit can be hit and die
+    /// </summary>
+    public class Unit : MonoBehaviour
     {
-        _data = data;
+        public int Life { get; private set; }
+        public int Damage => _data.Damage;
+        public int Range => _data.Range;
 
-        Life = data.Life;
-    }
+        [SerializeField] ActorView _view;
 
-    public void Move(Vector2 destination)
-    {
-        _destination = destination;
-        _view.PlayState("walk");
-    }
+        UnitData _data;
+        Vector2? _destination;
 
-    public void Update()
-    {
-        Move();
-    }
-
-    void Move()
-    {
-        if (_destination != null)
+        public void Initialize(UnitData data)
         {
-            Vector3 direction = _destination.Value - new Vector2(transform.position.x,transform.position.y);
-            float distance = direction.magnitude;
-            float moveStep = Time.deltaTime * _data.MoveSpeed;
-            direction.Normalize();
+            _data = data;
 
-            if ( moveStep >= distance)
-            {
-                transform.position = _destination.Value;
-                _destination = null;
-                _view.PlayState("idle");
-            }
-            else
-            {
-                transform.position = transform.position +  moveStep * direction;
-            }
-
-
+            Life = data.Life;
         }
-    }
 
+        public void Move(Vector2 destination)
+        {
+            _destination = destination;
+            _view.PlayState("walk");
+        }
+
+        public void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            if (_destination != null)
+            {
+                Vector3 direction = _destination.Value - new Vector2(transform.position.x,transform.position.y);
+                float distance = direction.magnitude;
+                float moveStep = Time.deltaTime * _data.MoveSpeed;
+                direction.Normalize();
+
+                if ( moveStep >= distance)
+                {
+                    transform.position = _destination.Value;
+                    _destination = null;
+                    _view.PlayState("idle");
+                }
+                else
+                {
+                    transform.position = transform.position +  moveStep * direction;
+                }
+
+
+            }
+        }
+
+    }
 }
--- Assets/Scripts/Tests/UnitTests.cs
+++ Assets/Scripts/Tests/UnitTests.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Gameplay.Units;
 using UnityEngine;
 
 public class UnitTests : MonoBehaviour
