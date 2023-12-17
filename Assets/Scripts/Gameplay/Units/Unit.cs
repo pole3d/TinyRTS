62a68cf0cdcd5e7bac6c6a9f2fe6e6bb1609b4f8
Mattéo Benaissa <92030744+MatteoBenaissaGit@users.noreply.github.com>
2023-12-17 21:25:13 +0100

    Namespaces changes

--- Assets/Scripts/Gameplay/Units/Unit.cs
+++ Assets/Scripts/Gameplay/Units/Unit.cs
@@ -1,71 +1,72 @@
-using System;
 using System.Collections.Generic;
 using Common.ActorSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-/// <summary>
-/// Represents a unit in the rts,
-/// the unit can respond to order, like moving
-/// a unit can be hit and die
-/// </summary>
-public class Unit : MonoBehaviour
+namespace Gameplay.Units
 {
-    public int Life { get; private set; }
-    public int Damage => _unitData.Damage;
-    public int Range => _unitData.Range;
-    public float MoveSpeed => _unitData.MoveSpeed;
-    public Sprite IconSprite => _unitData.IconSprite;
-    public List<UnitData.ActionType> Actions => _unitData.UnitActions;
-
-    [SerializeField] ActorView _view;
-    Vector2? _destination;
-
-    [Space (10)][Header("Unit Data")]
-    [SerializeField] GameplayData _data;
-    public UnitData _unitData;
-
-    private void Start()
+    /// <summary>
+    /// Represents a unit in the rts,
+    /// the unit can respond to order, like moving
+    /// a unit can be hit and die
+    /// </summary>
+    public class Unit : MonoBehaviour
     {
-        Initialize(_data.GetUnitData(_unitData.UnitType));
-    }
+        public int Life { get; private set; }
+        public int Damage => _unitData.Damage;
+        public int Range => _unitData.Range;
+        public float MoveSpeed => _unitData.MoveSpeed;
+        public Sprite IconSprite => _unitData.IconSprite;
+        public List<UnitData.ActionType> Actions => _unitData.UnitActions;
 
-    public void Initialize(UnitData data)
-    {
-        _unitData = data;
+        [SerializeField] ActorView _view;
+        Vector2? _destination;
 
-        Life = data.Life;
-    }
+        [Space (10)][Header("Unit Data")]
+        [SerializeField] GameplayData _data;
+        public UnitData _unitData;
 
-    public void Move(Vector2 destination)
-    {
-        _destination = destination;
-        _view.PlayState("walk");
-    }
-
-    public void Update()
-    {
-        Move();
-    }
-
-    void Move()
-    {
-        if (_destination != null)
+        private void Start()
         {
-            Vector3 direction = _destination.Value - new Vector2(transform.position.x, transform.position.y);
-            float distance = direction.magnitude;
-            float moveStep = Time.deltaTime * _unitData.MoveSpeed;
-            direction.Normalize();
+            Initialize(_data.GetUnitData(_unitData.UnitType));
+        }
 
-            if (moveStep >= distance)
+        public void Initialize(UnitData data)
+        {
+            _unitData = data;
+
+            Life = data.Life;
+        }
+
+        public void Move(Vector2 destination)
+        {
+            _destination = destination;
+            _view.PlayState("walk");
+        }
+
+        public void Update()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            if (_destination != null)
             {
-                transform.position = _destination.Value;
-                _destination = null;
-                _view.PlayState("idle");
-            }
-            else
-            {
-                transform.position = transform.position + moveStep * direction;
+                Vector3 direction = _destination.Value - new Vector2(transform.position.x, transform.position.y);
+                float distance = direction.magnitude;
+                float moveStep = Time.deltaTime * _unitData.MoveSpeed;
+                direction.Normalize();
+
+                if (moveStep >= distance)
+                {
+                    transform.position = _destination.Value;
+                    _destination = null;
+                    _view.PlayState("idle");
+                }
+                else
+                {
+                    transform.position = transform.position + moveStep * direction;
+                }
             }
         }
     }
