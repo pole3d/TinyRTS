83627fcdc88d838df9913b63088c2115dd98fdb1
P3D\nicolas.lepers <nicolaslepers176@gmail.com>
2023-12-18 17:33:16 +0100

    modif unit speed & add some unit

--- Assets/Scripts/GameManagement/BoardManager.cs
+++ Assets/Scripts/GameManagement/BoardManager.cs
@@ -9,25 +9,34 @@ namespace GameManagement
     public class BoardManager
     {
         private GameManager _gameManager;
-        
+
         public BoardManager(GameManager gameManager)
         {
             _gameManager = gameManager;
 
-            CreateTestUnitAt(Vector2.one*7);
-            CreateTestUnitAt(Vector2.one*8);
-            CreateTestUnitAt(Vector2.one*9);
-            CreateTestUnitAt(Vector2.one*10);
-            CreateTestUnitAt(Vector2.one*11);
-            CreateTestUnitAt(Vector2.one*12);
-            CreateTestUnitAt(Vector2.one*13);
-            CreateTestUnitAt(Vector2.one*14);
-            CreateTestUnitAt(Vector2.one*15);
+            CreateTestUnitAt(Vector2.one * 7);
+            CreateTestUnitAt(Vector2.one * 8);
+            CreateTestUnitAt(Vector2.one * 9);
+            CreateTestUnitAt(Vector2.one * 10);
+            CreateTestUnitAt(Vector2.one * 11);
+            CreateTestUnitAt(Vector2.one * 12);
+            CreateTestUnitAt(Vector2.one * 13);
+            CreateTestUnitAt(Vector2.one * 14);
+            CreateTestUnitAt(Vector2.one * 15);
+            CreateTestUnitAt(Vector2.one * 16);
+            CreateTestUnitAt(Vector2.one * 17);
+            CreateTestUnitAt(Vector2.one * 18);
+            CreateTestUnitAt(Vector2.one * 19);
+            CreateTestUnitAt(Vector2.one * 20);
+            CreateTestUnitAt(Vector2.one * 21);
+            CreateTestUnitAt(Vector2.one * 22);
+            CreateTestUnitAt(Vector2.one * 23);
+            CreateTestUnitAt(Vector2.one * 24);
         }
 
         public void Update()
         {
-            
+
         }
 
         private void CreateTestUnitAt(Vector2 position)
